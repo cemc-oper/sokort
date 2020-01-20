4091c06a76b7eed20a329e1da33188296289c904
perillaroc <perillaroc@gmail.com>
2020-01-20 08:33:57 +0800

    :sparkles: add k_sfc_an_aea.
    东亚区域K指数

---
+++ nwpc_graphics/systems/grapes_gfs_gmf/graphics/k_sfc_an_aea.py
@@ -0,0 +1,27 @@
+# coding: utf-8
+"""GRAPES GFS 东亚 K指数
+
+图片样例请访问NWPC/CMA官网：
+    http://nwpc.nmc.cn/list.jhtml?class_id=03130310
+"""
+
+import datetime
+
+import pytimeparse
+
+from nwpc_graphics.systems.grapes_gfs_gmf.plotter import BasePlotter
+
+
+class Plotter(BasePlotter):
+    def __init__(self, task: dict, work_dir: str, config: dict):
+        BasePlotter.__init__(self, task, work_dir, config)
+
+        self.ncl_script_name = "GFS_GRAPES_K_SFC_AN_AEA.ncl"
+
+    def _get_image_list(self):
+        forecast_timedelta = datetime.timedelta(
+            seconds=pytimeparse.parse(self.task["forecast_time"]))  # datetime.timedelta(hours=3)
+        forecast_hour = f"{int(forecast_timedelta.total_seconds()) // 3600:03}"
+        return [{
+            "path": f"./AEA_AN_{forecast_hour}.png"
+        }]
