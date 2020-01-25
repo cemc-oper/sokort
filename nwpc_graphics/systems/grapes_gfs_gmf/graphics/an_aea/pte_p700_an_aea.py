121dac4926e49af8447ee7860732e60ef2ca2c29
perillaroc <perillaroc@gmail.com>
2020-01-25 19:36:42 +0800

    :sparkles: add pte an aea.

---
+++ nwpc_graphics/systems/grapes_gfs_gmf/graphics/an_aea/pte_p700_an_aea.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+"""
+700hPa 假相当位温
+
+图片样例请访问NWPC/CMA官网：
+    http://nwpc.nmc.cn/list.jhtml?class_id=03130309
+"""
+
+from nwpc_graphics.systems.grapes_gfs_gmf.graphics.an_aea import AnAeaPlotter
+
+
+class Plotter(AnAeaPlotter):
+    plot_types = [
+        "pte_p700_an_aea"
+    ]
+
+    def __init__(self, task: dict, work_dir: str, config: dict):
+        AnAeaPlotter.__init__(self, task, work_dir, config)
+
+        self.ncl_script_name = "GFS_GRAPES_PTE_P700_AN_AEA.ncl"
