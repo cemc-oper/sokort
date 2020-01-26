947fdc9d18febcfce8201a622a189fb66dcda73f
perillaroc <perillaroc@gmail.com>
2020-01-26 19:38:23 +0800

    :sparkles: add mslp_sfc_fc_ahne.

---
+++ nwpc_graphics/systems/grapes_gfs_gmf/graphics/fc_ahne/mslp_sfc_fc_ahne.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+"""
+海平面气压
+
+图片样例请访问NWPC/CMA官网：
+    http://nwpc.nmc.cn/list.jhtml?class_id=03130105
+"""
+from nwpc_graphics.systems.grapes_gfs_gmf.graphics.fc_ahne import FcAhnePlotter
+
+
+class Plotter(FcAhnePlotter):
+    plot_types = [
+        "mslp_sfc_fc_ahne"
+    ]
+
+    def __init__(self, task: dict, work_dir: str, config: dict):
+        FcAhnePlotter.__init__(self, task, work_dir, config)
+
+        self.ncl_script_name = "GFS_GRAPES_MSLP_SFC_FC_AHNE.ncl"
