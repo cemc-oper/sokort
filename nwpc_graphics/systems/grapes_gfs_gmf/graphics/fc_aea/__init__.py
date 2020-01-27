a13174489a006c5c60829ee4f13a82fbc4086371
perillaroc <perillaroc@gmail.com>
2020-01-27 15:49:46 +0800

    :sparkles: add wind fc aea for 500, 700, 850 and 925hPa.

---
+++ nwpc_graphics/systems/grapes_gfs_gmf/graphics/fc_aea/__init__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+
+from nwpc_graphics.systems.grapes_gfs_gmf.plotter import BasePlotter
+
+
+class FcAeaPlotter(BasePlotter):
+    def __init__(self, task: dict, work_dir: str, config: dict):
+        BasePlotter.__init__(self, task, work_dir, config)
+
+        self.ncl_script_name = None
+
+    def _get_image_list(self):
+        forecast_hour = f"{int(self.forecast_timedelta.total_seconds()) // 3600:03}"
+        return [{
+            "path": f"./AEA_FC_{forecast_hour}.png"
+        }]
---
+++ nwpc_graphics/systems/grapes_gfs_gmf/graphics/fc_aea/wind_p500_fc_aea.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+"""
+500hPa 风场
+
+图片样例请访问NWPC/CMA官网：
+    http://nwpc.nmc.cn/list.jhtml?class_id=0313030403
+"""
+from nwpc_graphics.systems.grapes_gfs_gmf.graphics.fc_aea import FcAeaPlotter
+
+
+class Plotter(FcAeaPlotter):
+    plot_types = [
+        "wind_p500_fc_aea"
+    ]
+
+    def __init__(self, task: dict, work_dir: str, config: dict):
+        FcAeaPlotter.__init__(self, task, work_dir, config)
+
+        self.ncl_script_name = "GFS_GRAPES_WIND_P500_FC_AEA.ncl"
