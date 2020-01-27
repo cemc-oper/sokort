af17c2c99cf3f1265d229f4fdb3ebb1579f09326
perillaroc <perillaroc@gmail.com>
2020-01-27 17:02:57 +0800

    :sparkles: add rh_p850_wind_p850_fc_aea.

---
+++ nwpc_graphics/systems/grapes_gfs_gmf/graphics/fc_aea/rh_p850_wind_p850_fc_aea.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+"""
+850hPa 相对湿度 + 850hPa 风场
+
+图片样例请访问NWPC/CMA官网：
+    http://nwpc.nmc.cn/list.jhtml?class_id=03130308
+"""
+from nwpc_graphics.systems.grapes_gfs_gmf.graphics.fc_aea import FcAeaPlotter
+
+
+class Plotter(FcAeaPlotter):
+    plot_types = [
+        "rh_p850_wind_p850"
+    ]
+
+    def __init__(self, task: dict, work_dir: str, config: dict):
+        FcAeaPlotter.__init__(self, task, work_dir, config)
+
+        self.ncl_script_name = "GFS_GRAPES_RH_P850+WIND_P850_FC_AEA.ncl"
