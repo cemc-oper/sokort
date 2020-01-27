5e8cf456dccde60f0693157abb4263dbb0a7fba2
perillaroc <perillaroc@gmail.com>
2020-01-27 17:05:35 +0800

    :sparkles: add str_p700_fc_aea.

--- nwpc_graphics/systems/grapes_gfs_gmf/graphics/fc_aea/str_p700_fc_aea.py
+++ nwpc_graphics/systems/grapes_gfs_gmf/graphics/fc_aea/str_p700_fc_aea.py
@@ -1,19 +1,19 @@
 # coding: utf-8
 """
-850hPa 相对湿度 + 850hPa 风场
+700hPa 流场
 
 图片样例请访问NWPC/CMA官网：
-    http://nwpc.nmc.cn/list.jhtml?class_id=03130308
+    http://nwpc.nmc.cn/list.jhtml?class_id=03130309
 """
 from nwpc_graphics.systems.grapes_gfs_gmf.graphics.fc_aea import FcAeaPlotter
 
 
 class Plotter(FcAeaPlotter):
     plot_types = [
-        "rh_p850_wind_p850_fc_aea"
+        "str_p700_fc_aea"
     ]
 
     def __init__(self, task: dict, work_dir: str, config: dict):
         FcAeaPlotter.__init__(self, task, work_dir, config)
 
-        self.ncl_script_name = "GFS_GRAPES_RH_P850+WIND_P850_FC_AEA.ncl"
+        self.ncl_script_name = "GFS_GRAPES_STR_P700_FC_AEA.ncl"
