911c763a7f65feddaf686783864f1eb1e56f2b59
perillaroc <perillaroc@gmail.com>
2020-02-04 21:06:32 +0800

    :recycle: refactor BasePlotter.run_plot method.

--- nwpc_graphics/systems/grapes_gfs_gmf/plotter.py
+++ nwpc_graphics/systems/grapes_gfs_gmf/plotter.py
@@ -46,17 +46,20 @@ class BasePlotter(object):
         self.forecast_time_interval = 12
         self.forecast_data_format = "grib2"
         self.forecast_data_center = "ecmwf"
+        self.run_script_name = "run_ncl.sh"
+        self.load_env_script_name = "load_env.sh"
 
         # time options for task.
-        self.start_datetime = datetime.datetime.fromisoformat(
-            self.task["start_datetime"])  # datetime.datetime(2020, 1, 11, 0)
-        self.forecast_timedelta = datetime.timedelta(
-            seconds=pytimeparse.parse(self.task["forecast_time"]))  # datetime.timedelta(hours=3)
+
+        self.start_datetime = datetime.datetime.fromisoformat(self.task["start_datetime"])
+        # datetime.datetime, such as datetime.datetime(2020, 1, 11, 0)
+
+        self.forecast_timedelta = datetime.timedelta(seconds=pytimeparse.parse(self.task["forecast_time"]))
+        # datetime.timedelta, such as datetime.timedelta(hours=3)
+
         self.forecast_datetime = self.start_datetime + self.forecast_timedelta
 
-        # start_day = self.start_datetime.strftime("%Y%m%d")  # 20200111
         self.start_time = self.start_datetime.strftime("%Y%m%d%H")  # 2020011100
-        # start_hour = f"{self.start_datetime.hour:02}"  # 00
         self.forecast_hour = f"{int(self.forecast_timedelta.total_seconds()) // 3600:03}"  # 003
         self.min_forecast_time = self.forecast_hour
         self.max_forecast_time = self.forecast_hour
@@ -68,22 +71,29 @@ class BasePlotter(object):
         """
         if self.ncl_script_name is None:
             raise ValueError("ncl_script_name should be set.")
+        self._prepare_environment()
+        envs = self._generate_environ()
+        self._run_process(envs)
+
+    def show_plot(self):
+        """Show images in IPython.
+        """
+        image_list = self._get_image_list()
+        from IPython.display import Image, display
+        for an_image in image_list:
+            display(Image(filename=f"./{an_image['path']}"))
+
+    def _prepare_environment(self):
         ncl_script_name = self.ncl_script_name  # "GFS_GRAPES_PWAT_SFC_AN_AEA.ncl"
 
-        ncl_lib = self.config["ncl_lib"]  # "/home/wangdp/project/graph/ncllib"
-        geodiag_root = self.config["geodiag_root"]  # "/home/wangdp/project/graph/GEODIAG"
-        geodiag_tools = str(Path(geodiag_root, "tools"))
+        ncl_dir = self.task["ncl_dir"]
+        # str, "/home/wangdp/project/graph/operation/GMF_GRAPES_GFS_POST/tograph/script"
 
-        graphic_product_lib_root = ncl_lib
-
-        ncl_dir = self.task["ncl_dir"]  # "/home/wangdp/project/graph/operation/GMF_GRAPES_GFS_POST/tograph/script"
-        script_dir = self.task["script_dir"]  # "/home/wangdp/project/graph/operation/GMF_GRAPES_GFS_POST/tograph/script"
-
-        data_path = self.task["data_path"]  # "/sstorage1/COMMONDATA/OPER/NWPC/GRAPES_GFS_GMF/Prod-grib/2020011021/ORIG/"
+        script_dir = self.task["script_dir"]
+        # str "/home/wangdp/project/graph/operation/GMF_GRAPES_GFS_POST/tograph/script"
 
         # create environment
         Path(self.work_dir).mkdir(parents=True, exist_ok=True)
-
         os.chdir(self.work_dir)
 
         with open("grapes_meso_date", "w") as f:
@@ -94,26 +104,42 @@ class BasePlotter(object):
         shutil.copy2(f"{ncl_dir}/{ncl_script_name}", f"{ncl_script_name}")
 
         component_directory = Path(__file__).parent
-        script_path = "run_ncl.sh"
-        shutil.copy2(f"{str(Path(component_directory, script_path))}", script_path)
-        shutil.copy2(f"{str(Path(component_directory, 'load_env.sh'))}", "load_env.sh")
+        run_ncl_script_path = Path(component_directory, self.run_script_name)
+        load_env_script_path = Path(component_directory, self.load_env_script_name)
+        shutil.copy2(f"{str(run_ncl_script_path)}", self.run_script_name)
+        shutil.copy2(f"{str(load_env_script_path)}", self.load_env_script_name)
+
+    def _generate_environ(self):
+        ncl_script_name = self.ncl_script_name  # "GFS_GRAPES_PWAT_SFC_AN_AEA.ncl"
+
+        ncl_lib = self.config["ncl_lib"]  # "/home/wangdp/project/graph/ncllib"
+        graphic_product_lib_root = ncl_lib
+
+        geodiag_root = self.config["geodiag_root"]  # "/home/wangdp/project/graph/GEODIAG"
+        geodiag_tools = str(Path(geodiag_root, "tools"))
+
+        data_path = self.task["data_path"]
+        # str "/sstorage1/COMMONDATA/OPER/NWPC/GRAPES_GFS_GMF/Prod-grib/2020011021/ORIG/"
 
         envs = os.environ
         envs.update({
-                "GEODIAG_ROOT": geodiag_root,
-                "GEODIAG_TOOLS": geodiag_tools,
-                "GRAPHIC_PRODUCT_LIB_ROOT": graphic_product_lib_root,
-                "FORECAST_DATA_FORMAT": self.forecast_data_format,
-                "FORECAST_DATA_CENTER": self.forecast_data_center,
-                "start_time": self.start_time,
-                "min_forecast_time": self.min_forecast_time,
-                "max_forecast_time": self.max_forecast_time,
-                "forecast_time_interval": f"{self.forecast_time_interval}",
-                "data_path": data_path,
-                "ncl_script_name": ncl_script_name,
+            "GEODIAG_ROOT": geodiag_root,
+            "GEODIAG_TOOLS": geodiag_tools,
+            "GRAPHIC_PRODUCT_LIB_ROOT": graphic_product_lib_root,
+            "FORECAST_DATA_FORMAT": self.forecast_data_format,
+            "FORECAST_DATA_CENTER": self.forecast_data_center,
+            "start_time": self.start_time,
+            "min_forecast_time": self.min_forecast_time,
+            "max_forecast_time": self.max_forecast_time,
+            "forecast_time_interval": f"{self.forecast_time_interval}",
+            "data_path": data_path,
+            "ncl_script_name": ncl_script_name,
         })
+        return envs
+
+    def _run_process(self, envs: dict):
         pipe = subprocess.Popen(
-            [f"./{str(script_path)}"],
+            [f"./{self.run_script_name}"],
             start_new_session=True,
             env=envs
         )
@@ -121,14 +147,6 @@ class BasePlotter(object):
         stdout, stderr = pipe.communicate()
         pipe.wait()
         pipe.terminate()
-
-    def show_plot(self):
-        """Show images in IPython.
-        """
-        image_list = self._get_image_list()
-        from IPython.display import Image, display
-        for an_image in image_list:
-            display(Image(filename=f"./{an_image['path']}"))
 
     def _get_image_list(self):
         """Get image list.
