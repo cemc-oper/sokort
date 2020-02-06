60f6278c892f4b05bd35d350ee780a2792733081
perillaroc <perillaroc@gmail.com>
2020-02-06 18:53:14 +0800

    :fire: delete unused codes.

--- nwpc_graphics/systems/grapes_gfs_gmf/util.py
+++
@@ -1,7 +0,0 @@
-import pathlib
-
-from nwpc_graphics._util import load_plotters_from_paths
-
-if __name__ == "__main__":
-    plotters = load_plotters_from_paths([pathlib.Path(pathlib.Path(__file__).parent, "graphics")])
-    print(plotters)
