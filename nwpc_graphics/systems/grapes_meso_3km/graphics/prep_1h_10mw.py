5b172d570a9e1731ef8d9c3c736ed8a558a73ed4
perillaroc <perillaroc@gmail.com>
2020-02-06 11:18:40 +0800

    :bug: fix bug of forecast_hour.

--- nwpc_graphics/systems/grapes_meso_3km/graphics/prep_1h_10mw.py
+++ nwpc_graphics/systems/grapes_meso_3km/graphics/prep_1h_10mw.py
@@ -10,15 +10,15 @@ class Plotter(BasePlotter):
         BasePlotter.__init__(self, task, work_dir, config)
 
         self.ncl_script_name = "grapes_meso_prep_1hr_10mw.ncl"
-        self.forecast_hour = int(self.forecast_timedelta.total_seconds()) // 3600
 
         if not self._check_forecast_time():
             raise ValueError(f"forecast time must greater than 0h.")
 
     def _get_image_list(self):
         return [{
-            "path": f"GRAPES-MESOS-prep-{self.start_time}1hr_{self.forecast_hour:03}_China.png"
+            "path": f"GRAPES-MESOS-prep-{self.start_time}1hr_{self.forecast_hour}_China.png"
         }]
 
     def _check_forecast_time(self) -> bool:
-        return not self.forecast_hour == 0
+        forecast_hour = int(self.forecast_hour)
+        return not forecast_hour == 0
