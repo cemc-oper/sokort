53da9397056ef09017b33104f512d8e859f3f7b7
perillaroc <perillaroc@gmail.com>
2020-02-06 11:23:51 +0800

    :sparkles: add prep_10mw for 6h, 12h and 24h.

---
+++ nwpc_graphics/systems/grapes_meso_3km/graphics/prep_24h_10mw.py
@@ -0,0 +1,24 @@
+from nwpc_graphics.systems.grapes_meso_3km.plotter import BasePlotter
+
+
+class Plotter(BasePlotter):
+    plot_types = [
+        "prep_24h_10mw"
+    ]
+
+    def __init__(self, task: dict, work_dir: str, config: dict):
+        BasePlotter.__init__(self, task, work_dir, config)
+
+        self.ncl_script_name = "grapes_meso_prep_24hr_10mw.ncl"
+
+        if not self._check_forecast_time():
+            raise ValueError(f"forecast time must greater than 0h.")
+
+    def _get_image_list(self):
+        return [{
+            "path": f"GRAPES-MESOS-prep-{self.start_time}24hr_{self.forecast_hour}_China.png"
+        }]
+
+    def _check_forecast_time(self) -> bool:
+        forecast_hour = int(self.forecast_hour)
+        return (forecast_hour != 0) and (forecast_hour % 24 == 0)
