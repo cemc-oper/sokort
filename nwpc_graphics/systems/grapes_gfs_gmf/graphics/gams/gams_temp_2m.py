9505c942fb15943eb05a898515e93f4d95680f47
perillaroc <perillaroc@gmail.com>
2020-02-08 13:17:45 +0800

    :sparkles: add GAMS temp for 2m, 250hPa, 500hPa and 850hPa.

---
+++ nwpc_graphics/systems/grapes_gfs_gmf/graphics/gams/gams_temp_2m.py
@@ -0,0 +1,22 @@
+"""
+2m 温度
+
+图片暂时没有外网访问渠道。
+"""
+from nwpc_graphics.systems.grapes_gfs_gmf.graphics.gams import GamsPlotter
+
+
+class Plotter(GamsPlotter):
+    plot_types = [
+        "gams.temp_2m"
+    ]
+
+    def __init__(self, task: dict, work_dir: str, config: dict):
+        GamsPlotter.__init__(self, task, work_dir, config)
+
+        self.ncl_script_name = "gams_temp_2m.ncl"
+
+    def _get_image_list(self):
+        return [{
+            "path": f"./EAT_ASIA_L2M_P9_{self.start_time}00{self.forecast_hour}00.png"
+        }]
