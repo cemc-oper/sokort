ebc545e7e7ad0e13c34fb2bb3687ba29ce92a6af
perillaroc <perillaroc@gmail.com>
2020-02-08 13:30:30 +0800

    :sparkles: add GAMS wind for 10m, 250hPa, 500hPa, 700hPa 850hPa and 920hPa.

---
+++ nwpc_graphics/systems/grapes_gfs_gmf/graphics/gams/gams_wind_10m.py
@@ -0,0 +1,22 @@
+"""
+10m 风场
+
+图片暂时没有外网访问渠道。
+"""
+from nwpc_graphics.systems.grapes_gfs_gmf.graphics.gams import GamsPlotter
+
+
+class Plotter(GamsPlotter):
+    plot_types = [
+        "gams.wind_10m"
+    ]
+
+    def __init__(self, task: dict, work_dir: str, config: dict):
+        GamsPlotter.__init__(self, task, work_dir, config)
+
+        self.ncl_script_name = "gams_wind_10m.ncl"
+
+    def _get_image_list(self):
+        return [{
+            "path": f"./EDA_ASIA_L10M_P9_{self.start_time}00{self.forecast_hour}00.png"
+        }]
