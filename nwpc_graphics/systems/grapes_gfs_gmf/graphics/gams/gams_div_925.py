b3f4d8fd8b6580743eb1995dda6b6f94d8d92f9e
perillaroc <perillaroc@gmail.com>
2020-02-08 10:43:32 +0800

    :sparkles: add GAMS div for 700 and 925hPa.

---
+++ nwpc_graphics/systems/grapes_gfs_gmf/graphics/gams/gams_div_925.py
@@ -0,0 +1,22 @@
+"""
+925hPa 散度
+
+图片暂时没有外网访问渠道。
+"""
+from nwpc_graphics.systems.grapes_gfs_gmf.graphics.gams import GamsPlotter
+
+
+class Plotter(GamsPlotter):
+    plot_types = [
+        "gams.div_925"
+    ]
+
+    def __init__(self, task: dict, work_dir: str, config: dict):
+        GamsPlotter.__init__(self, task, work_dir, config)
+
+        self.ncl_script_name = "gams_div_925.ncl"
+
+    def _get_image_list(self):
+        return [{
+            "path": f"./EDI_ASIA_L92_P9_{self.start_time}00{self.forecast_hour}00.png"
+        }]
