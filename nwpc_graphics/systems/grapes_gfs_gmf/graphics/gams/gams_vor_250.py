b822e7e8a1ef95201b2b233c4c92c2b24ef8a641
perillaroc <perillaroc@gmail.com>
2020-02-08 13:23:09 +0800

    :sparkles: add GAMS vorticity for 250hPa, 700hPa and 920hPa.

---
+++ nwpc_graphics/systems/grapes_gfs_gmf/graphics/gams/gams_vor_250.py
@@ -0,0 +1,22 @@
+"""
+250hPa 涡度
+
+图片暂时没有外网访问渠道。
+"""
+from nwpc_graphics.systems.grapes_gfs_gmf.graphics.gams import GamsPlotter
+
+
+class Plotter(GamsPlotter):
+    plot_types = [
+        "gams.vor_250"
+    ]
+
+    def __init__(self, task: dict, work_dir: str, config: dict):
+        GamsPlotter.__init__(self, task, work_dir, config)
+
+        self.ncl_script_name = "gams_vor_250.ncl"
+
+    def _get_image_list(self):
+        return [{
+            "path": f"./ERV_ASIA_L25_P9_{self.start_time}00{self.forecast_hour}00.png"
+        }]
