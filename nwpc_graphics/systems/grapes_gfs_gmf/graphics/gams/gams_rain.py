bd87453fe5c5887bd482511636d4b0aad3362864
perillaroc <perillaroc@gmail.com>
2020-02-08 13:32:51 +0800

    :sparkles: add GAMS rain.

---
+++ nwpc_graphics/systems/grapes_gfs_gmf/graphics/gams/gams_rain.py
@@ -0,0 +1,22 @@
+"""
+累积降水
+
+图片暂时没有外网访问渠道。
+"""
+from nwpc_graphics.systems.grapes_gfs_gmf.graphics.gams import GamsPlotter
+
+
+class Plotter(GamsPlotter):
+    plot_types = [
+        "gams.rain"
+    ]
+
+    def __init__(self, task: dict, work_dir: str, config: dict):
+        GamsPlotter.__init__(self, task, work_dir, config)
+
+        self.ncl_script_name = "gams_rain.ncl"
+
+    def _get_image_list(self):
+        return [{
+            "path": f"./EE0_ASIA_L88_P9_{self.start_time}00{self.forecast_hour}00.png"
+        }]
