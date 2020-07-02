0607e92b6542a07fea8f73e29fe741bbb28a862b
perillaroc <perillaroc@gmail.com>
2020-07-02 16:23:29 +0800

    :sparkles: add cn_500height_mslp

---
+++ nwpc_graphics/systems/grapes_meso_3km/graphics/cn_500height_mslp.py
@@ -0,0 +1,30 @@
+"""
+500 hPa 高度场 + 海平面气压
+
+图片样例请访问 NMC 官网：
+    http://www.nmc.cn/publish/area/china/h500p.html
+"""
+from nwpc_graphics.systems.grapes_meso_3km import SystemPlotter
+
+
+class Plotter(SystemPlotter):
+    plot_types = [
+        "cn_500height_850wind"
+    ]
+
+    def __init__(self, task: dict, work_dir: str, config: dict):
+        SystemPlotter.__init__(self, task, work_dir, config)
+
+        self.ncl_script_name = "grapes_meso_3km_500_height_mslp.ncl"
+
+        if not self._check_forecast_time():
+            raise ValueError(f"forecast time must greater than 0h.")
+
+    def get_image_list(self):
+        return [{
+            "path": f"GRAPES_MESO-3KM-500hPa-height-mslp-{self.start_time}_{self.forecast_hour}.png"
+        }]
+
+    def _check_forecast_time(self) -> bool:
+        forecast_hour = int(self.forecast_hour)
+        return not forecast_hour == 0
