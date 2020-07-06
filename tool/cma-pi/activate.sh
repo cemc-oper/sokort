9057bd5a57effbe8905f11cb338acc88de30ffb5
perillaroc <perillaroc@gmail.com>
2020-07-06 10:48:18 +0800

    :package: add config files for CMA-PI HPC.

---
+++ tool/cma-pi/activate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#-------------------------------------
+# activate environment for CMA-PI
+#-------------------------------------
+
+set -x
+source /g1/u/wangdp/start_anaconda3.sh
+conda activate nwpc-data
+export NWPC_GRAPHICS_CONFIG="/g3/wangdp/project/work/graphics/nwpc-graphics/tool/cma-pi/config/config.yaml"
+
+#module load mathlib/ncl_ncarg/6.5.0/gnu
+
+set +x
