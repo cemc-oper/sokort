5e3322b814dc6f0e3d22b27b7a0c9c5ee6139e9f
perillaroc <perillaroc@gmail.com>
2020-07-08 14:36:46 +0800

    :alien: use new ncl script.

--- nwpc_graphics/systems/grapes_meso_3km/graphics/meso_3km/diag1_reg_radar.py
+++ nwpc_graphics/systems/grapes_meso_3km/graphics/meso_3km/diag1_reg_radar.py
@@ -15,7 +15,7 @@ class Plotter(Meso3kmPlotter):
     def __init__(self, task: dict, work_dir: str, config: dict):
         Meso3kmPlotter.__init__(self, task, work_dir, config)
 
-        self.ncl_script_name = "grapes_meso_3km_reg_radar.ncl"
+        self.ncl_script_name = "grapes_meso_reg_radar.ncl"
 
     def get_image_list(self):
         forecast_hour = f"{int(self.forecast_timedelta.total_seconds()) // 3600:03}"
