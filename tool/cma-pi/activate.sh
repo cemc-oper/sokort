db986d2e4bbbbcfb664b9ed99d889d0f2abf8958
perillaroc <perillaroc@gmail.com>
2020-11-08 10:19:33 +0800

    :alien: user directory has been moved.

--- tool/cma-pi/activate.sh
+++ tool/cma-pi/activate.sh
@@ -6,7 +6,7 @@
 set -x
 source /g1/u/wangdp/start_anaconda3.sh
 conda activate nwpc-data
-export NWPC_GRAPHICS_CONFIG="/g3/wangdp/project/work/graphics/nwpc-graphics/tool/cma-pi/config/config.yaml"
+export NWPC_GRAPHICS_CONFIG="/g11/wangdp/project/work/graphics/nwpc-graphics/tool/cma-pi/config/config.yaml"
 
 #module load mathlib/ncl_ncarg/6.5.0/gnu
 
