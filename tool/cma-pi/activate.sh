52b5402c1f4643e66543153d54a15bc6021e99ea
perillaroc <perillaroc@gmail.com>
2021-06-23 09:28:25 +0800

    :art: update config tool for CMA-PI.

--- tool/cma-pi/activate.sh
+++ tool/cma-pi/activate.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #-------------------------------------
 # activate environment for CMA-PI
+# Use python environment Anaconda3 installed by wangdp.
 #-------------------------------------
 
 set -x
 source /g1/u/wangdp/start_anaconda3.sh
-conda activate nwpc-data
-export NWPC_GRAPHICS_CONFIG="/g11/wangdp/project/work/graphics/nwpc-graphics/sokort/tool/cma-pi/config/config.yaml"
+conda activate py39-dev
+SOKORT_ROOT="/g11/wangdp/project/work/graphics/nwpc-graphics/sokort"
+export NWPC_GRAPHICS_CONFIG="${SOKORT_ROOT}/tool/cma-pi/config/config.yaml"
 
-#module load mathlib/ncl_ncarg/6.5.0/gnu
+# module load mathlib/ncl_ncarg/6.5.0/gnu
 
 set +x
---
+++ tool/cma-pi/config/load_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#-------------------------------------
+# load NCL environment in CMA-PI.
+#-------------------------------------
+
+set -x
+module load mathlib/ncl_ncarg/6.5.0/gnu
+set +x
