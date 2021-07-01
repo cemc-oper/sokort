8faa3ff370d9307af048ee85aa3bbe25acd7f3be
perillaroc <perillaroc@gmail.com>
2021-07-01 18:02:16 +0800

    :art: set additional envs and link files for cape 850wind plot.

--- sokort/systems/grapes_meso_3km/graphics/meso_diag/area_cape_850wind.py
+++ sokort/systems/grapes_meso_3km/graphics/meso_diag/area_cape_850wind.py
@@ -4,6 +4,10 @@ CAPE + 850 hPa 风场
 图片样例请访问 NWPC/CMA 官网：
     http://nwpc.nmc.cn/list.jhtml?class_id=0302160314
 """
+from pathlib import Path
+import shutil
+import os
+
 from sokort.systems.grapes_meso_3km import SystemPlotter
 
 
@@ -62,3 +66,14 @@ class Plotter(SystemPlotter):
 
     def _check_forecast_time(self) -> bool:
         return True
+
+    def _prepare_environment(self):
+        super(Plotter, self)._prepare_environment()
+
+        script_dir = self.task["script_dir"]
+        shutil.copy2(f"{script_dir}/grapes_meso_cape_850wind.rgb", "grapes_meso_cape_850wind.rgb")
+
+    def _generate_environ(self):
+        envs = super(Plotter, self)._generate_environ()
+        envs["NCARG_COLORMAPS"] = os.path.expandvars("${NCARG_ROOT}/lib/ncarg/colormaps:.")
+        return envs
