b1690fd49e5c7de29707f28a4e240679d1b96f2b
perillaroc <perillaroc@gmail.com>
2021-10-11 14:32:35 +0800

    :bookmark: bump to v0.4.1

--- sokort/__init__.py
+++ sokort/__init__.py
@@ -2,4 +2,4 @@ from sokort.config import get_config, load_config
 from sokort._logging import get_logger
 from sokort.interface import draw_plot, show_plot
 
-__version__ = "0.4.1-alpha"
+__version__ = "0.4.1"
