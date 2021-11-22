eef0a3900f1db0b0a637d5b5b2aa0a7ccd2e963d
perillaroc <perillaroc@gmail.com>
2021-11-22 16:15:01 +0800

    :sparkles: add div plots for CMA-MESO.

---
+++ sokort/systems/grapes_meso_3km/graphics/meso_diag/area_850hPa_qv_div.py
@@ -0,0 +1,26 @@
+"""
+K 指数 + 850 hPa 风场
+
+图片样例请访问 NWPC/CMA 官网：
+    http://nwpc.nmc.cn/list.jhtml?class_id=0302160307
+"""
+from sokort.systems.grapes_meso_3km import SystemPlotter, AREA_LIST
+
+
+class Plotter(SystemPlotter):
+    plot_types = [
+        "area_850_div_wind"
+    ]
+
+    def __init__(self, task: dict, work_dir: str, config: dict, **kwargs):
+        SystemPlotter.__init__(self, task, work_dir, config, **kwargs)
+
+        self.ncl_script_name = "grapes_meso_reg_850hPa_qv_div.ncl"
+
+    def get_image_list(self):
+        return [
+            {
+                "name": area["name"],
+                "path": f"GRAPES-MESOS-reg-850hPa-qv-div-{area['image_path_name']}{self.start_time}_{self.forecast_hour}.png"
+            } for area in AREA_LIST
+        ]
