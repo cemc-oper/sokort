f12dabb0034c6e5788690ad9d2c2a823c5356d4b
perillaroc <perillaroc@gmail.com>
2021-11-24 15:33:00 +0800

    :art: use README as long description.

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from setuptools import setup, find_packages
+import codecs
+from os import path
 import io
 import re
 
 with io.open("sokort/__init__.py", "rt", encoding="utf8") as f:
     version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
+here = path.abspath(path.dirname(__file__))
+
+with codecs.open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='sokort',
@@ -11,7 +18,8 @@ setup(
     version=version,
 
     description='CEMC Graphics Script Tool.',
-    long_description=__doc__,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
     url='https://github.com/nwpc-oper/sokort',
     author='perillaroc',
