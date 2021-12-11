c2248b7bf45db63bb22585c1ad52a0be5b2866df
perillaroc <perillaroc@gmail.com>
2021-12-11 11:35:11 +0800

    :bug: fix input variable.

--- sokort/convert_image.sh
+++ sokort/convert_image.sh
@@ -10,7 +10,7 @@
 #-------------------
 set -x
 INPUT=$1
-convert -density 200 "${INPUT[0]}" plot_${INPUT%.*}.png
+convert -density 200 "${INPUT}[0]" plot_${INPUT%.*}.png
 convert -trim plot_${INPUT%.*}.png trim1_${INPUT%.*}.png
 convert -trim trim1_${INPUT%.*}.png ${INPUT%.*}.png
 rm plot_${INPUT%.*}.png
