9bfd0e12e1a47aaab036ed6559064026a87dc7df
perillaroc <perillaroc@gmail.com>
2023-05-19 20:27:14 +0800

    :package: embed python-versioneer for version.

--- sokort/__init__.py
+++ sokort/__init__.py
@@ -2,4 +2,5 @@ from sokort.config import get_config, load_config
 from sokort._logging import get_logger
 from sokort.interface import draw_plot, show_plot
 
-__version__ = "2023.05.0"
+from . import _version
+__version__ = _version.get_versions()['version']
